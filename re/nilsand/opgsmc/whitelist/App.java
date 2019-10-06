d9330ae0316189204a15701b9a92ca2d1f80ff50
Nils ANDRÉ-CHANG <nils@nilsand.re>
2019-10-06 15:11:16 +0100

    Fix indentation in `App.java`

--- re/nilsand/opgsmc/whitelist/App.java
+++ re/nilsand/opgsmc/whitelist/App.java
@@ -1,11 +1,10 @@
 package re.nilsand.opgsmc.whitelist;
 
 public class App extends org.bukkit.plugin.java.JavaPlugin {
-
 	static {
 		System.loadLibrary("http_server");
 	}
 
-    @Override
-    public native void onEnable();
+	@Override
+	public native void onEnable();
 }
